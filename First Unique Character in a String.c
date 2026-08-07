0d2743871a0736bc38ad9f96dd7d9e3fd1a0b51d
Jeni0201 <jjenifer0201@gmail.com>
2026-08-07 11:38:16 +0530

    Implement function to find first unique character

---
+++ First Unique Character in a String.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+int firstUniqChar(char* s) {
+    int count[26] = {0};
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        count[s[i] - 'a']++;
+    }
+    for (int i = 0; i < len; i++) {
+        if (count[s[i] - 'a'] == 1) {
+            return i;
+        }
+    }
+    return -1;
+}
